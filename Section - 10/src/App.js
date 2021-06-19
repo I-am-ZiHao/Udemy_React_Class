6c404a271c730330eecc30b3a2ea925dcb5802b9
I-am-ZiHao <e64031025@gmail.com>
2021-06-19 17:18:39 +0800

    complete section 10

--- Section - 10/src/App.js
+++ Section - 10/src/App.js
@@ -1,29 +1,49 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState, useContext } from 'react';
+import AuthContext from './components/store/auth-context';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
+  const context = useContext(AuthContext);
 
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // useEffect( () => {
+  //   const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+  //   if (storedUserLoggedInInformation === '1') {
+  //     setIsLoggedIn(true);
+  //   }
+  // }, [])
+
+  // const loginHandler = (email, password) => {
+  //   // We should of course check email and password
+  //   // But it's just a dummy/ demo anyways
+  //   localStorage.setItem('isLoggedIn', 1);
+  //   setIsLoggedIn(true);
+  // };
+
+  // const logoutHandler = () => {
+  //   localStorage.removeItem('isLoggedIn');
+  //   setIsLoggedIn(false);
+  // };
 
   return (
     <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
+      {/* <AuthContext.Provider value={
+        {
+          isLoggedIn: isLoggedIn,
+          onLogout: logoutHandler
+        }
+      }> */}
+        <MainHeader />
+        <main>
+          {!context.isLoggedIn && <Login />}
+          {context.isLoggedIn && <Home />}
+        </main>
+      {/* </AuthContext.Provider> */}
     </React.Fragment>
   );
 }
---
+++ Section - 10/src/components/UI/Input/Input.js
@@ -0,0 +1,42 @@
+import classes from './Input.module.css';
+import React, { useRef, useEffect, useImperativeHandle } from 'react';
+
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate
+    };
+  })
+
+  // useEffect(() => {
+  //   inputRef.current.focus();
+  // }, []);
+
+  return (
+    <div
+      className={`${classes.control} ${
+        // emailIsValid === false ? classes.invalid : ''
+        // emailState.isValid === false ? classes.invalid : ''
+        props.isValid === false ? classes.invalid : ''
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
+
+export default Input;
